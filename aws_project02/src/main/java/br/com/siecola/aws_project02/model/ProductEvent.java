8158b88aae6394988604696f67b41fee12d62aa2
Wagner Santos <wbatista985@gmail.com>
2024-06-19 19:21:47 -0300

    feat - adicionando product event sns

---
+++ aws_project02/src/main/java/br/com/siecola/aws_project02/model/ProductEvent.java
@@ -0,0 +1,13 @@
+package br.com.siecola.aws_project02.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductEvent {
+
+    private long productId;
+    private String code;
+    private String username;
+}
