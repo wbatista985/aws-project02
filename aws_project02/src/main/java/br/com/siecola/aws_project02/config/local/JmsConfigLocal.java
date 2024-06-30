38d314e711f53675a4faa2985d0c28ab0091b616
Wagner Santos <wbatista985@gmail.com>
2024-06-30 14:13:35 -0300

    feat - remove import unsed

--- aws_project02/src/main/java/br/com/siecola/aws_project02/config/local/JmsConfigLocal.java
+++ aws_project02/src/main/java/br/com/siecola/aws_project02/config/local/JmsConfigLocal.java
@@ -6,7 +6,6 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSClient;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
